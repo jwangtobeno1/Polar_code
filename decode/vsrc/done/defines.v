4c44513a69e06dff93720d1ea7f66b496f449937
WangJie <xduwangjie@gmail.com>
2023-09-27 21:22:10 +0800

    daily: modify defines.v

--- decode/vsrc/done/defines.v
+++ decode/vsrc/done/defines.v
@@ -1,11 +1,14 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
+// 码率设置
 `define BIT_N 256
 `define BIT_K 128
 
+// 量化设置
 `define LLR_CHANNEL_LEN 4
 `define LLR_INTERNAL_LEN 6
 
+// PROCESS_UNIT 接口位宽设置
 `define PROCESS_UNIT_LLR_NUM 16
 `define PROCESS_UNIT_LLR_BUS (`LLR_INTERNAL_LEN*`PROCESS_UNIT_LLR_NUM)-1:0 //95:0
 `define PROCESS_UNIT_LLR_OUT_BUS (`LLR_INTERNAL_LEN*`PROCESS_UNIT_LLR_NUM)/2-1:0//47:0
@@ -21,32 +24,35 @@
 `define FUNC_SPC4_LLR_BUS 23:0
 `define FUNC_SPC4_BIT_BUS 3:0
 
+// 指令数量
 `define CNT_INST_MAX 209
+
+// 指令定义
 `define INST_BUS 11:0 //11:8(llr&bit mem_addr) : 7:0(PU inst)
 
-`define PU_INST_BUS 7:0 //4(operation) :3(stage) :1(left or right) 
+`define PU_INST_BUS 7:0 //4(operation) :3(stage) :1(left or right)
 `define PU_OPCODE_BUS 3:0
 `define INST_STAGE_BUS 2:0
 `define PU_OP_LOCATION 7:4
 `define INST_STAGE_LOCATION 3:1
-`define FUNC_F      4'b0000
-`define FUNC_G      4'b0001
-`define FUNC_REP    4'b0010
-`define FUNC_SPC    4'b0011
-`define FUNC_RATE0  4'b0100
-`define FUNC_RATE1  4'b0101
-`define FUNC_TYPE1  4'b0110
-`define FUNC_TYPE3  4'b0111
-`define FUNC_G0R    4'b1001
+`define FUNC_F 4'b0000
+`define FUNC_G 4'b0001
+`define FUNC_REP 4'b0010
+`define FUNC_SPC 4'b0011
+`define FUNC_RATE0 4'b0100
+`define FUNC_RATE1 4'b0101
+`define FUNC_TYPE1 4'b0110
+`define FUNC_TYPE3 4'b0111
+`define FUNC_G0R 4'b1001
 `define BIT_COMBINE 4'b1010
 `define BIT_COMBINE_0R 4'b1100
 
-`define REVERSE      3'd7
-`define COMB2TO4     3'd6
-`define COMB4TO8     3'd0
-`define COMB8TO16    3'd1
-`define COMB16TO32   3'd2
-`define COMB32TO64   3'd3
-`define COMB64TO128  3'd4
+// 阶段定义
+`define REVERSE 3'd7
+`define COMB2TO4 3'd6
+`define COMB4TO8 3'd0
+`define COMB8TO16 3'd1
+`define COMB16TO32 3'd2
+`define COMB32TO64 3'd3
+`define COMB64TO128 3'd4
 `define COMB128TO256 3'd5
-
--- decode/vsrc/done/func_fg.v
+++ decode/vsrc/done/func_fg.v
@@ -2,7 +2,7 @@
 //Filename                : func_fg.v
 //Author                  : WangJie
 //Created On              : 2022-12-04 22:25
-//Last Modified           : 
+//Last Modified           :
 //Description             : 8 operations of f-function or g-function
 //
 // the relation of llr and llr_arr:
@@ -16,48 +16,58 @@
 // llr_arr[2] and llr_arr[3] => dout[1]
 //
 // llr[95 : 0] => 96bit = 16 * 6bit
-// 
+//
 //======================================================
 `include "defines.v"
 
-module func_fg(
-    input   wire    [`PROCESS_UNIT_LLR_BUS]     llr, //96bit
-    input   wire    [`FUNC_G_BIT_LEN-1:0]       bit_in, //8bit
-    input   wire                                flag_fg,  //1 -> f;   0 -> g;
-    output  wire    [`PORCESS_UNIT_LLR_OUT_BUS]    llr_out //48bit
+module func_fg (
+    input  wire [    `PROCESS_UNIT_LLR_BUS] llr,      //96bit
+    input  wire [      `FUNC_G_BIT_LEN-1:0] bit_in,   //8bit
+    input  wire                             flag_fg,  //1 -> f;   0 -> g;
+    output wire [`PORCESS_UNIT_LLR_OUT_BUS] llr_out   //48bit
 );
 
-wire [`LLR_INTERNAL_LEN-1:0] llr_arr  [`PROCESS_UNIT_LLR_NUM-1:0];
-wire [`LLR_INTERNAL_LEN-1:0] dout_arr [`FUNC_G_BIT_LEN-1:0];
-wire [0:0]                   s_arr    [`FUNC_G_BIT_LEN-1:0];
+  wire [`LLR_INTERNAL_LEN-1:0] llr_arr [`PROCESS_UNIT_LLR_NUM-1:0];
+  wire [`LLR_INTERNAL_LEN-1:0] dout_arr[      `FUNC_G_BIT_LEN-1:0];
+  wire [                  0:0] s_arr   [      `FUNC_G_BIT_LEN-1:0];
 
-generate
+  generate
     genvar i;
-    for(i = 0; i < `PROCESS_UNIT_LLR_NUM; i = i+1) begin : gen_scale
-        assign llr_arr[i] = llr[(`PROCESS_UNIT_LLR_NUM*`LLR_INTERNAL_LEN-i*`LLR_INTERNAL_LEN)-1 -:`LLR_INTERNAL_LEN];
+    for (i = 0; i < `PROCESS_UNIT_LLR_NUM; i = i + 1) begin : gen_scale
+      assign llr_arr[i] = llr[(`PROCESS_UNIT_LLR_NUM*`LLR_INTERNAL_LEN-i*`LLR_INTERNAL_LEN)-1
+        -:`LLR_INTERNAL_LEN];
     end
-endgenerate
+  endgenerate
 
-generate
+  generate
     genvar s_i;
-    for(s_i=0; s_i < `FUNC_G_BIT_LEN; s_i = s_i+1) begin : gen_si
-        assign s_arr[s_i] = bit_in[`FUNC_G_BIT_LEN-1-s_i];
+    for (s_i = 0; s_i < `FUNC_G_BIT_LEN; s_i = s_i + 1) begin : gen_si
+      assign s_arr[s_i] = bit_in[`FUNC_G_BIT_LEN-1-s_i];
     end
-endgenerate
+  endgenerate
 
-generate
+  generate
     genvar j;
-    for(j = 0; j < `PROCESS_UNIT_LLR_NUM; j = j+2) begin : gen_scale_j
-        pe pe_inst_j(
-            .llr_a(llr_arr[j]),
-            .llr_b(llr_arr[j+1]),
-            .flag(flag_fg),
-            .s(s_arr[j/2]),
-            .dout(dout_arr[j/2])
-        );
+    for (j = 0; j < `PROCESS_UNIT_LLR_NUM; j = j + 2) begin : gen_scale_j
+      pe pe_inst_j (
+          .llr_a(llr_arr[j]),
+          .llr_b(llr_arr[j+1]),
+          .flag(flag_fg),
+          .s(s_arr[j/2]),
+          .dout(dout_arr[j/2])
+      );
     end
-endgenerate
+  endgenerate
 
-assign llr_out = {dout_arr[0],dout_arr[1],dout_arr[2],dout_arr[3],dout_arr[4],dout_arr[5],dout_arr[6],dout_arr[7]};
+  assign llr_out = {
+    dout_arr[0],
+    dout_arr[1],
+    dout_arr[2],
+    dout_arr[3],
+    dout_arr[4],
+    dout_arr[5],
+    dout_arr[6],
+    dout_arr[7]
+  };
 
 endmodule
